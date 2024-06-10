eab499ff8570856c1d29ca6ccfbe1dd0315ede25
TylerRake7468 <lalitpalariya450@gmail.com>
2024-06-10 21:59:12 +0530

    Added toggle stimulus js for profile button

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
+
+
+export default class extends Controller {
+  static targets = ['openUserMenu', 'dropdown']
+
+  connect() {
+    this.openUserMenuTarget.addEventListener("click", (e)=>{
+        openDropDown(this.dropdownTarget)
+    })
+  }
+}
+
+function openDropDown(element) {
+    toggle(element).then(() => {
+        console.log("Enter transition complete")
+    })
+}
+ 
+// remove element when close
+function closeDropDown(element) {
+    leave(element).then(() => {
+        element.destroy();
+    })
+}
