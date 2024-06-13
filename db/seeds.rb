8299d6f228b05805e372f4744b8a2b30d7366a8b
TylerRake7468 <lalitpalariya450@gmail.com>
2024-06-13 22:43:41 +0530

    Added property table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.create!([
+    {
+        name: "Marwad Villa",
+        headline: "Marwadi Vila, Marwad Junction",
+        description: "asfkaj jojasdv jj o js naj sdj nsdvk njkjd ",
+        address_1: "Suryanagar",
+        address_2: "Marwad junction",
+        city: "Pali",
+        state: "Rajasthan",
+        country: "India"
+    },
+    {
+        name: "Palariya Villa",
+        headline: "Palariya Vila, Marwad Junction",
+        description: "asfsaf asf askjs aksjd kaj jojasdv jj o js naj sdj nsdvk njkjd ",
+        address_1: "Suryanagar",
+        address_2: "New Marwad junction",
+        city: "Pali",
+        state: "Rajasthan",
+        country: "India"
+    }
+])
