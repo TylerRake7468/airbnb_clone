163c5497272df4ce727c928e6bf3ca72c38a5307
TylerRake7468 <lalitpalariya450@gmail.com>
2024-06-15 10:39:08 +0530

    Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-Property.create!([
-    {
-        name: "Marwad Villa",
-        headline: "Marwadi Vila, Marwad Junction",
-        description: "asfkaj jojasdv jj o js naj sdj nsdvk njkjd ",
-        address_1: "Suryanagar",
-        address_2: "Marwad junction",
-        city: "Pali",
-        state: "Rajasthan",
-        country: "India"
-    },
-    {
-        name: "Palariya Villa",
-        headline: "Palariya Vila, Marwad Junction",
-        description: "asfsaf asf askjs aksjd kaj jojasdv jj o js naj sdj nsdvk njkjd ",
-        address_1: "Suryanagar",
-        address_2: "New Marwad junction",
-        city: "Pali",
-        state: "Rajasthan",
-        country: "India"
-    }
-])
+20.times do
+    Property.create!([
+        name: Faker::Lorem.unique.sentence(word_count: 3),
+        headline: Faker::Lorem.unique.sentence(word_count: 6),
+        description: Faker::Lorem.paragraph(sentence_count: 10),
+        address_1: Faker::Address.street_name,
+        address_2: Faker::Address.street_address,
+        city: Faker::Address.city_prefix,
+        state: Faker::Address.state,
+        country: Faker::Address.country
+    ])
+end
