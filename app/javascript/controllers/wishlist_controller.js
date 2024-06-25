1e4d57ecac2eab3a8230330d18a9fef0244995de
TylerRake7468 <lalitpalariya450@gmail.com>
2024-06-25 22:12:26 +0530

    wishlist icon fix on dashboard and on show page

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -1,70 +1,80 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    updateWishlistOnClick(){
-        const isUserLoggedIn = this.element.dataset.userLoggedIn;
-        if(isUserLoggedIn === "false"){
-            document.querySelector(".js-login").click();
-            return;
-        }
-        const userId = this.element.dataset.userId;
-        const propertyId = this.element.dataset.propertyId;
-        
-        if(this.element.dataset.status == "false"){
-            this.updateWishlistStatus(propertyId, userId);
-        }else{
-            console.log("destroy action")
-            const wishlistId = this.element.dataset.wishlistId;
-            this.removePropertyFromWishlist(wishlistId);
-        }
+
+  static targets = ['icon', 'text'];
+
+  updateWishlistStatus(e) {
+    e.preventDefault();
+    const isUserLoggedIn = this.element.dataset.userLoggedIn;
+    if(isUserLoggedIn === "false"){
+      document.querySelector(".js-login").click();
+      return
     }
 
-    updateWishlistStatus(propertyId, userId){
-        const params = {
-            user_id: userId,
-            property_id: propertyId
-        };
-        const options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(params),
-        };
-        fetch('/api/wishlists', options)
-            .then(response => {
-              if (!response.ok) {
-                console.log('Network response was not ok', response.status);
-              }
-              return response.json();
-            })
-            .then(data => {
-                console.log("success", data)
-                this.element.dataset.wishlistId = data.id
-                this.element.classList.remove("fill-none")
-                this.element.classList.add("fill-primary")
-                this.element.dataset.status = "true"
-            })
-            .catch(error => {
-              console.error('Error:', error);
-            });
-        }
-    removePropertyFromWishlist(wishlistId){
-        const options = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-        fetch('/api/wishlists/'+wishlistId, options)
-            .then(response => {
-                this.element.dataset.wishlistId = ''
-                this.element.classList.remove("fill-primary")
-                this.element.classList.add("fill-none")
-                this.element.dataset.status = "false"
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-        }
+    if (this.element.dataset.status === "false"){
+      const propertyId = this.element.dataset.propertyId;
+      const userId = this.element.dataset.userId;
+      this.addPropertyToWishlist(propertyId, userId)
+    }
+    else {
+      const wishlistId = this.element.dataset.wishlistId;
+      this.removePropertyFromWishlist(wishlistId)
+    }
+  }
+
+  addPropertyToWishlist(propertyId, userId){
+    const params = {
+      property_id: propertyId,
+      user_id: userId,
+    };
+      
+    const options = {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    };
+
+    fetch('/api/wishlists', options)
+    .then(response => {
+      if (!response.ok) {
+        console.log(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      this.element.dataset.wishlistId = data.id;
+      this.element.dataset.status = "true";
+      this.iconTarget.classList.remove("fill-none");
+      this.iconTarget.classList.add("fill-primary");
+      
+      if (this.textTarget) {
+        this.textTarget.innerText = 'Saved';
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+  removePropertyFromWishlist(wishlistId) {
+    fetch('/api/wishlists/' + wishlistId, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      this.element.dataset.wishlistId = '';
+      this.element.dataset.status = "false";
+      this.iconTarget.classList.remove("fill-primary");
+      this.iconTarget.classList.add("fill-none");
+
+      if (this.textTarget) {
+        this.textTarget.innerText = 'Save';
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
 }
