55a1a16856a96118b1e43b0d0d52f192686e8685
TylerRake7468 <lalitpalariya450@gmail.com>
2024-07-17 22:24:10 +0530

    created booking page date picker and its jS backend

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -1,34 +1,58 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
 
 export default class extends Controller {
   
-    static targets = ["baseFare","numberOfNights", "serviceFee", "totalAmount"];
-    SERVICE_FEE = 0.18;
+    static targets = ["checkout","checkin","baseFare","numberOfNights", "serviceFee", "totalAmount"];
+    SERVICE_FEE_PERCENTAGE = 0.18;
 
     connect() {
+        flatpickr(this.checkinTarget, {
+            minDate: new Date().fp_incr(1),
+            onChange: (selectedDates, dateStr, instance) => {
+                this.triggerCheckoutDatePickr(selectedDates);
+            }
+        });
         this.updateDetails();
     }
 
+    triggerCheckoutDatePickr(selectedDates){
+        flatpickr(this.checkoutTarget, {
+            minDate: new Date(selectedDates).fp_incr(1),
+            onChange: (selectedDates, dateStr, instance) => {
+                this.updateDetails();
+            }
+        });
+        this.checkoutTarget.click();
+    }
+
     updateDetails(){
-        this.numberOfNightsTarget.textContent = this.numberOfNights;
-        this.baseFareTarget.textContent = this.calculateBaseFare;  
-        this.serviceFeeTarget.textContent = this.calculateServiceFee;
-        this.totalAmountTarget.textContent = this.calculateTotalAmount;
+        const nightsCount = this.numberOfNights();
+        const baseFareValue = this.calculateBaseFare(nightsCount);
+        const serviceFeeValue = this.calculateServiceFee(baseFareValue);
+        const totalAmount = this.calculateTotalAmount(baseFareValue, serviceFeeValue);
+        
+        this.numberOfNightsTarget.textContent = nightsCount;
+        this.baseFareTarget.textContent = baseFareValue;  
+        this.serviceFeeTarget.textContent = serviceFeeValue;
+        this.totalAmountTarget.textContent = totalAmount;
     }
 
-    get calculateBaseFare(){
-        return parseFloat(this.numberOfNights * this.element.dataset.perNightPrice).toFixed(2);
+    calculateBaseFare(nightsCount){
+        return parseFloat(nightsCount * this.element.dataset.perNightPrice).toFixed(2);
     }
 
-    get calculateServiceFee(){
-        return parseFloat(this.calculateBaseFare * this.SERVICE_FEE).toFixed(2);
+    calculateServiceFee(baseFareValue){
+        return parseFloat(baseFareValue * this.SERVICE_FEE_PERCENTAGE).toFixed(2);
     }
 
-    get calculateTotalAmount(){
-        return (parseFloat(this.calculateBaseFare) + parseFloat(this.calculateServiceFee)).toFixed(2);
+    calculateTotalAmount(baseFareValue, serviceFeeValue){
+        return (parseFloat(baseFareValue) + parseFloat(serviceFeeValue)).toFixed(2);
     }
 
-    get numberOfNights(){
-        return 4;
+    numberOfNights(){
+        const checkinDate = new Date(this.checkinTarget.value);
+        const checkoutDate = new Date(this.checkoutTarget.value);
+        return (checkoutDate - checkinDate)/(1000*60*60*24);
     }
 }
