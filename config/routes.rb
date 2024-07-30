da97af91d7b637721eeafb6530de380188ad8609
TylerRake7468 <lalitpalariya450@gmail.com>
2024-07-30 23:06:40 +0530

    Added profile template and button

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
 
   get "booking_payments/success", to: "booking_payments#success"
   resources :wishlists, only: [:index]
+  resources :profiles, only: [:show]
 end
