e347fbefef57928d16127a58b5640270a19906e4
TylerRake7468 <lalitpalariya450@gmail.com>
2024-08-09 23:48:59 +0530

    initial commit for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :wishlists, only: [:index]
   resources :profiles, only: [:index]
   resources :reservations, only: [:index]
+  resources :reviews, only: [:new]
 end
