894c356fe474fd51a8d63411f1f63f123ccefc36
TylerRake7468 <lalitpalariya450@gmail.com>
2024-08-12 23:57:40 +0530

    added partial code of rating

---
+++ app/javascript/controllers/rating_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+
+    setRating(e){
+        const rating = e.currentTarget.dataset.rating;
+        console.log("rating is:", rating);
+    }
+}
