3dd18298ad65e71ccade500f6822e2e2184d3bed
TylerRake7468 <lalitpalariya450@gmail.com>
2024-08-14 23:57:49 +0530

    added star functionality in rating

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -5,8 +5,25 @@ export default class extends Controller {
 
     setRating(e){
         const rating = e.currentTarget.dataset.rating;
-        const ratingInput = e.currentTarget.parentNode.querySelector('input[data-rating-target="cleanliness_rating"]');
-        
+        const category = e.currentTarget.dataset.category;
+        const rootElement = e.currentTarget.parentNode;
+        console.log("root ele:", rootElement);
+        const ratingInput = rootElement.querySelector(`input[data-rating-target="${category}"]`);
+
+        const stars = rootElement.querySelectorAll('[data-rating-target="star"]');
+        console.log("stars::", stars);
+        stars.forEach(star => {
+            console.log("star:::::", star);
+            if( star.dataset.rating <= rating){
+                star.classList.remove("fill-gray-300");
+                star.classList.add("fill-amber-300");
+            }else{
+                star.classList.remove("fill-amber-300");
+                star.classList.add("fill-gray-300");
+            }
+            
+        });
+
         if(ratingInput){
             ratingInput.value = rating;
         }
