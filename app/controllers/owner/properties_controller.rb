83dffacce43a5f934623cfbd8ad5be84306b9640
TylerRake7468 <lalitpalariya450@gmail.com>
2024-08-29 23:56:21 +0530

    Added index page in owner property page

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -2,7 +2,13 @@ module Owner
     class PropertiesController < ApplicationController
         before_action :authenticate_user!
         def index
+            @properties = current_user.properties
+        end
 
+        def new
+        end
+
+        def edit
         end
     end
 end
