486789eb0d3ca3953113b0e7ec8b0f2c0702d121
TylerRake7468 <lalitpalariya450@gmail.com>
2024-08-31 12:47:10 +0530

    Added edit page on owner property

--- app/controllers/owner/dashboard_controller.rb
+++ app/controllers/owner/dashboard_controller.rb
@@ -2,7 +2,7 @@ module Owner
     class DashboardController < ApplicationController
         before_action :authenticate_user!
         def index
-
+            @reservations = []
         end
     end
 end
--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -1,6 +1,8 @@
 module Owner
     class PropertiesController < ApplicationController
         before_action :authenticate_user!
+        before_action :set_property, only: [:edit, :update]
+
         def index
             @properties = current_user.properties
         end
@@ -9,6 +11,33 @@ module Owner
         end
 
         def edit
+        end
+
+        def update
+            if @property.update!(property_params)
+                redirect_to edit_owner_property_path, notice: "property update successfully."
+            else
+                redirect_back fallback_location: edit_owner_property_path, alert: "Failed to update profile"
+            end
+        end
+
+        private
+        def set_property
+            @property = current_user.properties.find(params[:id])
+        end
+
+        def property_params
+            params.require(:property).permit(
+                :name,
+                :headline,
+                :price,
+                :description,
+                :address_1,
+                :address_2,
+                :city,
+                :state,
+                :country_code
+            )
         end
     end
 end
